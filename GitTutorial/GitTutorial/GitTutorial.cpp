a2ea2623ec418e4706944086755e78ba518aa5aa
WeiWeiBigBear <ff1298tw@gmail.com>
2017-03-03 09:31:45 +0800

    Wayne

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -15,7 +15,8 @@ int main(array<System::String ^> ^args)
 {
 int guess;
 do {
-std::cin >> guess;
+	std::cout << "Choose a number between 1 - 100 :";
+	std::cin >> guess;
 } while (!Guess(guess));
 return 0;
 }
